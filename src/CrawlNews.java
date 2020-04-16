ecfc1c60c06708ecbe96bba5faf93e881654b441
linhhm2406 <linh.hm24@gmail.com>
2020-04-16 14:56:33 +0700

    add file

---
+++ src/CrawlNews.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CrawlNews {
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("https://dantri.com.vn/the-gioi.htm");
+            Scanner scanner = new Scanner(new InputStreamReader(url.openStream(),"UTF-8"));
+            scanner.useDelimiter("\\Z");
+            String content = "";
+            if (scanner.hasNext()) {
+                content = scanner.next();
+            }
+            scanner.close();
+
+            content = content.replaceAll("\\n+", "");
+
+            Pattern p = Pattern.compile("<a title=\"(.*?)\"\\s(.*?)newsdetail(.*?)</a>");
+            Matcher m = p.matcher(content);
+            while (m.find()) {
+                System.out.println(m.group(1));
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
